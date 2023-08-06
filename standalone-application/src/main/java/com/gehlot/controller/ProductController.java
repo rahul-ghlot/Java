e81ac10048a3977b603cea8380d04ee7f4a5925b
shubhgehlot <shubhgehlot0@gmail.com>
2023-08-07 04:46:33 +0530

    third commit

--- standalone-application/src/main/java/com/gehlot/controller/ProductController.java
+++ standalone-application/src/main/java/com/gehlot/controller/ProductController.java
@@ -2,6 +2,8 @@ package com.gehlot.controller;
 
 import com.gehlot.entity.Product;
 import com.gehlot.service.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,18 +15,19 @@ import java.util.List;
 @RestController
 public class ProductController {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
     @Autowired
     ProductService service;
 
     @GetMapping("/products")
-    public List<Product> getAllProducts(){
+    public List<Product> getAllProducts() {
         return service.getAllProducts();
     }
 
     @PostMapping("/product")
-    public String createProduct(@RequestBody Product product){
+    public String createProduct(@RequestBody Product product) {
+        log.debug("Post request for create product {}", product);
         service.createProduct(product);
-       return "product successfully created";
+        return "product successfully created";
     }
-
 }
