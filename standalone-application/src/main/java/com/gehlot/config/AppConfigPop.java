88ddf20d31085bd6962611f1ebd6cb4491e463bf
shubhgehlot <shubhgehlot0@gmail.com>
2023-08-13 10:07:15 +0530

    config changes

--- standalone-application/src/main/java/com/gehlot/config/AppConfigPop.java
+++ standalone-application/src/main/java/com/gehlot/config/AppConfigPop.java
@@ -1,2 +1,25 @@
-package com.gehlot.config;public class AppConfigPop {
+package com.gehlot.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.constraints.NotEmpty;
+
+@Configuration
+@ConfigurationProperties (prefix = "msg")
+@PropertySource("classpath:app-config.properties")
+@Validated
+public class AppConfigPop {
+
+    @NotEmpty
+    private String hello;
+
+    public String getHello() {
+        return hello;
+    }
+
+    public void setHello(String hello) {
+        this.hello = hello;
+    }
 }
