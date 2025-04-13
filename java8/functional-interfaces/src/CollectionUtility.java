e6c61f15360a23a93fd4d01e5f5af425e82232bc
shubhgehlot <shubhgehlot0@gmail.com>
2025-04-13 10:20:17 +0530

    Iterate Map

---
+++ java8/functional-interfaces/src/CollectionUtility.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class CollectionUtility {
+    public static Map<Integer, String> testMap() {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "Virat");
+        map.put(2, "Rohit");
+        map.put(3, "Dhoni");
+        map.put(4, "Sachin");
+        map.put(5, "Sehwag");
+        return map;
+    }
+}
---
+++ java8/functional-interfaces/src/IterateMap.java
@@ -0,0 +1,8 @@
+import java.util.Map;
+
+public class IterateMap {
+    public static void main(String[] args) {
+       Map<Integer, String> map = CollectionUtility.testMap();
+
+    }
+}
